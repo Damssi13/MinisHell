838154a9353725bba8085eb0884089c91646ae1d
bjandri <bzaza066@gmail.com>
2024-07-07 10:52:39 +0100

    push the fixed code

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: bjandri <bjandri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 15:09:51 by bjandri           #+#    #+#             */
-/*   Updated: 2024/07/06 10:08:54 by bjandri          ###   ########.fr       */
+/*   Updated: 2024/07/07 10:41:23 by bjandri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,11 @@ int	main(void)
 		if (!rl)
 			break ;
 		first_parse(rl, &head);
+		while(head)
+		{
+			printf("word [%s]  type[%i] index[%d] \n", head->token, head->type, head->index);
+			head = head->next;
+		}
 		free_tokens(head);
 		head = NULL;
 	}
