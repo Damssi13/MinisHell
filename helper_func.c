4379a7ef139712a28b31fcdc623544d590000e60
BrahimJandri <bzaza066@gmail.com>
2024-07-21 09:19:32 +0100

    add function that removes empty quotes

--- helper_func.c
+++ helper_func.c
@@ -6,7 +6,7 @@
 /*   By: bjandri <bjandri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 10:03:32 by bjandri           #+#    #+#             */
-/*   Updated: 2024/07/16 15:15:43 by bjandri          ###   ########.fr       */
+/*   Updated: 2024/07/21 09:10:34 by bjandri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,4 +81,82 @@ char *ft_strtok(char *str, const char *delim)
         s++;
     }
     return begin;
+}
+
+void remove_quotes(char *str)
+{
+    char *src;
+    char *dst;
+    int i;
+    int j;
+
+    i = 0;
+    src = str;
+    dst = str;
+    if (src[i] == '\'' || src[i] == '"')
+        i++;
+    j = 0;
+    while (src[i])
+        dst[j++] = src[i++];
+    if (j > 0 && (dst[j - 1] == '\'' || dst[j - 1] == '"'))
+        j--;
+    dst[j] = '\0';
+}
+
+int is_n_flag(char *arg)
+{
+    int i = 1;
+
+    if (arg[0] != '-')
+        return 0;
+    if (arg[1] == '\0')
+        return 0;
+    while (arg[i] != '\0')
+    {
+        if (arg[i] != 'n')
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+void	free_parser(t_parser *head)
+{
+	t_parser *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
+
+char *rm_quote(char *str)
+{
+    int i = 0;
+    int j = 0;
+    
+    char *dst = str;
+
+    while (str[i])
+    {
+        if (str[i] == '\'' || str[i] == '"' )
+        {
+            if (str[i + 1] == '\'' || str[i + 1] == '"')
+                i += 2;
+            else if (str[i + 1] == ' ' || str[i + 1] == '\t' || str[i + 1] == '\n')
+            {
+                while(str[i + 1] == ' ' || str[i + 1] == '\t' || str[i + 1] == '\n')
+                    i++;
+            }
+            else
+                dst[j++] = str[i++];
+        }
+        else
+            dst[j++] = str[i++];
+    }
+    dst[j] = '\0';
+    return str;
 }
